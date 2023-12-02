93255ad82406b97b982c9aa33872d521d23e79f0
Jeff Wong <42687471+jeff-yc-wong@users.noreply.github.com>
2023-12-01 21:40:01 -1000

    added triange dropdown icon for module list

--- src/pages/ModuleList/ModuleList.jsx
+++ src/pages/ModuleList/ModuleList.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import './ModuleList.css';
+import React, { useState } from "react";
+import "./ModuleList.css";
 
 const ModulesList = () => {
   const [expandedModules, setExpandedModules] = useState([]);
@@ -7,39 +7,41 @@ const ModulesList = () => {
   const modules = [
     {
       id: 1,
-      title: '1: Intro to Python',
+      title: "1: Hello World",
       submodules: [
         {
           id: 101,
-          title: '1.1: Setup',
+          title: "1.1: Welcome",
           subsubmodules: [
-            { id: 1001, title: '1.1.1: Hello World' },
-            { id: 1002, title: '1.1.2: Variables' },
+            { id: 1001, title: "1.1.1: Hello World" },
+            { id: 1002, title: "1.1.2: Variables" },
           ],
         },
-        { id: 102, title: '1.2: Input/Output(I/O)', 
-        subsubmodules: [
-          { id: 1003, title: '1.2.1: Output' },
-          { id: 1004, title: '1.2.2: Input' },
-        ],
-      },
+        {
+          id: 102,
+          title: "1.2: Comments",
+          subsubmodules: [
+            { id: 1003, title: "1.2.1: Output" },
+            { id: 1004, title: "1.2.2: Input" },
+          ],
+        },
       ],
     },
     {
       id: 2,
-      title: '2: Control Flow',
+      title: "2: Control Flow",
       submodules: [
-        { id: 201, title: '2.1: Conditional Math' },
-        { id: 202, title: '2.2: If/Else Statement' },
+        { id: 201, title: "2.1: Conditional Math" },
+        { id: 202, title: "2.2: If/Else Statement" },
       ],
     },
     // Add more modules and submodules as needed
   ];
 
   const handleModuleClick = (moduleId) => {
-    setExpandedModules(prevState => {
+    setExpandedModules((prevState) => {
       if (prevState && prevState.includes(moduleId)) {
-        return prevState.filter(id => id !== moduleId);
+        return prevState.filter((id) => id !== moduleId);
       } else {
         return [...(prevState || []), moduleId];
       }
@@ -50,37 +52,45 @@ const ModulesList = () => {
     <div className="container mt-5">
       <h1 className="title">Modules List</h1>
       <ul className="list-group">
-        {modules.map(module => (
+        {modules.map((module) => (
           <li key={module.id} className="list-group-item">
             <div
-              style={{ cursor: 'pointer' }}
+              style={{ cursor: "pointer" }}
               onClick={() => handleModuleClick(module.id)}
             >
               {module.title}
               <span className="triangle">
-                {expandedModules.includes(module.id) ? '▼' : '▶'}
+                {expandedModules.includes(module.id) ? "▼" : "▶"}
               </span>
             </div>
             {expandedModules.includes(module.id) && (
               <ul className="list-group mt-2">
-                {module.submodules.map(submodule => (
+                {module.submodules.map((submodule) => (
                   <li key={submodule.id} className="list-group-item">
                     <div
-                      style={{ cursor: 'pointer' }}
+                      style={{ cursor: "pointer" }}
                       onClick={() => handleModuleClick(submodule.id)}
                     >
                       {submodule.title}
-                      
+                      {"subsubmodules" in submodule && (
+                        <span className="triangle">
+                          {expandedModules.includes(submodule.id) ? "▼" : "▶"}
+                        </span>
+                      )}
                     </div>
-                    {expandedModules.includes(submodule.id) && (
-                      <ul className="list-group mt-2">
-                        {submodule.subsubmodules.map(subsubmodule => (
-                          <li key={subsubmodule.id} className="list-group-item">
-                            {subsubmodule.title}
-                          </li>
-                        ))}
-                      </ul>
-                    )}
+                    {expandedModules.includes(submodule.id) &&
+                      "subsubmodules" in submodule && (
+                        <ul className="list-group mt-2">
+                          {submodule.subsubmodules.map((subsubmodule) => (
+                            <li
+                              key={subsubmodule.id}
+                              className="list-group-item"
+                            >
+                              {subsubmodule.title}
+                            </li>
+                          ))}
+                        </ul>
+                      )}
                   </li>
                 ))}
               </ul>
@@ -90,6 +100,6 @@ const ModulesList = () => {
       </ul>
     </div>
   );
-}
+};
 
 export default ModulesList;
